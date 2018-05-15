4167fe616bee705369fb285fbd177445b79e8bdf
kuzdras <39298603+kuzdras@users.noreply.github.com>
2018-05-15 15:26:24 +0200

    Update Enemy.py

--- Enemy.py
+++ Enemy.py
@@ -2,18 +2,16 @@
 
 #super class
 class Enemy:
-    def __init__(self, name, hp, damage):
-        self.name = name
-        self.hp = hp
-        self.damage = damage
+  def __init__(self, name, hp, damage):
+    self.name = name
+    self.hp = hp
+    self.damage = damage
    
-    def is_alive(self):
-        return self.hp > 0
+  def is_alive(self):
+    return self.hp > 0
 
-  #time for some sub-classes
+#time for some sub-classes
   
-  class SpaceSpider(Enemy):
-    def __init__(self):
-        super().__init__(name = "Space Spider", hp = 6, damage = 1)
-       
-  
+class SpaceSpider(Enemy):
+  def __init__(self):
+    super().__init__(name = "Space Spider", hp = 6, damage = 1)
