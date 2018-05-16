8fa0e5fedf5f92646a5825760882498bcbfc0242
kuzdras <39298603+kuzdras@users.noreply.github.com>
2018-05-16 09:32:02 +0200

    Update Enemy.py

--- Enemy.py
+++ Enemy.py
@@ -2,10 +2,16 @@
 
 #super class
 class Enemy:
-  def __init__(self, name, hp, damage):
+  def __init__(self, name, hp, damage, description):
     self.name = name
     self.hp = hp
     self.damage = damage
+    self.description = description
+    
+   def desc(self):
+    self.name = name
+    self.description = description
+    return "{}\n {}\n".format(self.name, self.description) 
    
   def is_alive(self):
     return self.hp > 0
@@ -14,4 +20,8 @@ class Enemy:
   
 class SpaceSpider(Enemy):
   def __init__(self):
-    super().__init__(name = "Space Spider", hp = 6, damage = 1)
+    super().__init__(name = "Space Spider", hp = 6, damage = 1,
+                    description = "Green, dog-size spider. It has a eight red eyes and radioactive glow. It usualy lives on the planet surface")
+    
+    
+class Hopper
