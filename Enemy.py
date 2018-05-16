47f3ed85224eb2fbbbe842840c07da1e75e3d2d3
kuzdras <39298603+kuzdras@users.noreply.github.com>
2018-05-16 14:57:10 +0200

    Update Enemy.py

--- Enemy.py
+++ Enemy.py
@@ -18,7 +18,7 @@ class Enemy:
 
 #time for some sub-classes
   
-class SpaceSpider(Enemy):
+class Space_Spider(Enemy):
   def __init__(self):
     super().__init__(name = "Space Spider", hp = 6, damage = 1,
                     description = "Green, dog-size spider. It has a eight red eyes and radioactive glow. It usualy lives on the planet surface")
@@ -28,5 +28,13 @@ class Hopper(Enemy):
   def __init__(self):
     super.__init__(name = "Hopper",
                   hp = 10,
-                  damage = 3
+                  damage = 3,
                   description = "Giant insect. It's has massive back legs and razor-sharp front ones.")
+    
+class Broken_Droid(Enemy):
+  def __init__(self):
+     super.__init__(name = "Malfunctioned Security Droid",
+                  hp = 20,
+                  damage = 7,
+                  description = "Bulky Security Droid. It seems that it's malfunctioned because he perceives you as a threat.")
+    
